a1c06916b2d628eab483c12b0b1094f18a95c220
John Polacek <johnpolacek@gmail.com>
2011-08-10 07:01:21 -0500

    Decided to go with single quote over double

--- _/js/app.js
+++ _/js/app.js
@@ -44,7 +44,7 @@ var App = (function() {
         var pageID = '#'+pageName;
         
         // empty content
-        $("#content").empty();
+        $('#content').empty();
         
         if (!pageData) {
             pageData = {};
@@ -71,105 +71,105 @@ var App = (function() {
     function setPageEvents() {
         
         // set global events
-        $("#wrapper")
-            .delegate("#button-signout", "click", function(e){
+        $('#wrapper')
+            .delegate('#button-signout', 'click', function(e){
                 e.preventDefault();
                 userName = null;
                 userEmail = null;
                 setCookie('userName', '', 0);
                 enableSignout(false);
-                changePage("welcome", "Welcome");
+                changePage('welcome', 'Welcome');
             });
         
         // set page events
-        $("#content")
+        $('#content')
         
             // WELCOME PAGE
                 
-            .delegate("#signin-email", "focus", function(){
-                if ($(this).val() == "Email Address")
+            .delegate('#signin-email', 'focus', function(){
+                if ($(this).val() == 'Email Address')
                 {
-                    $(this).val("");
+                    $(this).val('');
                 }
             })
-            .delegate("#signin-email", "blur", function(){
-                if ($(this).val() == "")
+            .delegate('#signin-email', 'blur', function(){
+                if ($(this).val() == '')
                 {
-                    $(this).val("Email Address");
+                    $(this).val('Email Address');
                 }
             })
-            .delegate("#signin-submit", "click", function(e){
+            .delegate('#signin-submit', 'click', function(e){
                 e.preventDefault();
                 $('#form-welcome').submit();
             })
-            .delegate("#form-welcome", "submit", function(e){
+            .delegate('#form-welcome', 'submit', function(e){
                 e.preventDefault();
                 userEmail = $('#signin-email').val();
                     
                 if (validateEmail(userEmail)) {
                     // in a real app, you would communicate with an api
                     // to check if user exists, for example:
-                    // $.post("api/user/", { action:"check", email:userEmail }, function(data) {
+                    // $.post('api/user/', { action:'check', email:userEmail }, function(data) {
                     //     handleUserSubmit(data);
-                    // }, "json");
+                    // }, 'json');
                     //
                     handleEmailSubmit({email:userEmail});
                 } else {
-                    $(".error-text").css("visibility","visible");
+                    $('.error-text').css('visibility','visible');
                 }
             })
             
             // REGISTER PAGE
             
-            .delegate("#register-name", "focus", function(){
-                if ($(this).val() == "User Name")
+            .delegate('#register-name', 'focus', function(){
+                if ($(this).val() == 'User Name')
                 {
-                    $(this).val("");
+                    $(this).val('');
                 }
             })
-            .delegate("#register-name", "blur", function(){
-                if ($(this).val() == "")
+            .delegate('#register-name', 'blur', function(){
+                if ($(this).val() == '')
                 {
-                    $(this).val("User Name");
+                    $(this).val('User Name');
                 }
             })
-            .delegate("#form-register", "submit", function(e){
+            .delegate('#form-register', 'submit', function(e){
                 e.preventDefault();
                 var registerEmail = $('#register-email').val();
                 var registerName = $('#register-name').val();
                 
                 // hide any visible errors
-                $("#error-email").css("visibility","hidden");
-                $("#error-name").css("visibility","hidden");
-                $("#error-confirm").css("visibility","hidden");
+                $('#error-email').css('visibility','hidden');
+                $('#error-name').css('visibility','hidden');
+                $('#error-confirm').css('visibility','hidden');
                 
                 // validate register form
                 var registerValid = true;
                 
                 if (!validateEmail(registerEmail)) {
                     registerValid = false;
-                    $("#error-email").css("visibility","visible");
+                    $('#error-email').css('visibility','visible');
                 }
                 
                 if (!validateName(registerName)) {
                     registerValid = false;
-                    $("#error-name").css("visibility","visible");
+                    $('#error-name').css('visibility','visible');
                 }
                 
                 if (!$('#register-confirm').is(':checked')) {
                     registerValid = false;
-                    $("#error-confirm").css("visibility","visible");
+                    $('#error-confirm').css('visibility','visible');
                 }
                 
                 if (registerValid) {
                     userName = registerName;
                     userEmail = registerEmail;
                     enableSignout(true);
-                    setCookie("userName", userName);
-                    changePage("home","Thanks For Signing Up", {userName:registerName})
+                    setCookie('userName', userName);
+                    changePage('home','Thanks For Signing Up', {userName:registerName})
                 }
             })
-            .delegate("#register-submit", "click", function(e){
+            .delegate('#register-submit', 'click', function(e){
                 e.preventDefault();
                 $('#form-register').submit();
             });
@@ -177,9 +177,9 @@ var App = (function() {
     
     function enableSignout(enable) {
         if (enable) {
-            $("#button-signout").show();
+            $('#button-signout').show();
         } else {
-            $("#button-signout").hide();
+            $('#button-signout').hide();
         }
     }
     
@@ -192,10 +192,10 @@ var App = (function() {
         // in a real app, this function would handle user data returned from the api
         if (!data || !data.userName) {
             // user doesn't exist, so go to register
-            changePage("register","Create Account",data)
+            changePage('register','Create Account',data)
         } else {
             // user does exist, so go to home
-            changePage("home","Thanks For Signing Up",data)
+            changePage('home','Thanks For Signing Up',data)
         }
     }
     
@@ -238,13 +238,13 @@ var App = (function() {
     //--------------------------------------
     
     function getCookie(cookieName) {
-        var i, x, y, cookies = document.cookie.split(";");
-        var cookieValue = "";
+        var i, x, y, cookies = document.cookie.split(';');
+        var cookieValue = '';
         for (i = 0; i < cookies.length; i++)
         {
-            x = cookies[i].substr(0, cookies[i].indexOf("="));
-            y = cookies[i].substr(cookies[i].indexOf("=")+1);
-            x = x.replace(/^\s+|\s+$/g,"");
+            x = cookies[i].substr(0, cookies[i].indexOf('='));
+            y = cookies[i].substr(cookies[i].indexOf('=')+1);
+            x = x.replace(/^\s+|\s+$/g,'');
             if (x == cookieName) {
                 cookieValue = unescape(y);
             }
@@ -255,8 +255,8 @@ var App = (function() {
     function setCookie(cookieName, value, expDays) {
         var expDate = new Date();
         expDate.setDate(expDate.getDate() + expDays);
-        var cookieValue = escape(value) + ((expDays === null) ? "" : "; expires="+expDate.toUTCString());
-        document.cookie = cookieName + "=" + cookieValue;
+        var cookieValue = escape(value) + ((expDays === null) ? '' : '; expires='+expDate.toUTCString());
+        document.cookie = cookieName + '=' + cookieValue;
     }
     
     return {
@@ -267,14 +267,14 @@ var App = (function() {
             
             // use jQuery address for app navigation
             $.address.init(function(event) {
-                userName = getCookie("userName");
+                userName = getCookie('userName');
                 if (userName) {
                     // if user is signed in go home
-                    changePage("home","Welcome Back")
+                    changePage('home','Welcome Back')
                 } else {
                     // otherwise, go to signin screen (welcome)
                     enableSignout(false);
-                    changePage("welcome","Welcome")
+                    changePage('welcome','Welcome')
                 }  
             })
                 .change(function(event) {
